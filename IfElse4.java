a22b9baeb6dc7b4fd22d2317c35ed18af2c544d6
William <wroquequi@unsa.edu.pe>
2023-05-07 16:05:51 -0500

    Ejercicio 4 de Java If Else

---
+++ IfElse4.java
@@ -0,0 +1,17 @@
+public class IfElse4
+{
+    public static void main(String args[]) {
+            int x=50;
+            int y=50;
+            if(x==y){
+                    System.out.println("1");
+            } else if (x>y){
+		    System.out.println("2");
+	    }
+	    else {
+		    System.out.println("3");
+	    }
+
+    }
+}
+
