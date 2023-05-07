1b5a520cae18f1826c458fe8479c5535365af0ed
William <wroquequi@unsa.edu.pe>
2023-05-07 16:00:42 -0500

    Ejercicio 2 de Java If Else

---
+++ IfElse2.java
@@ -0,0 +1,12 @@
+public class IfElse2
+{
+    public static void main(String args[]) {
+            int x=50;
+            int y=50;
+            if(x==y){
+                    System.out.println("Hello World");
+            }
+
+    }
+}
+
