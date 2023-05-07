089545b4f23939c76b1f232bebb2efae73a03703
William <wroquequi@unsa.edu.pe>
2023-05-07 16:16:57 -0500

    Ejercicio 2 de Java Switch

---
+++ Switch2.java
@@ -0,0 +1,20 @@
+public class Switch2
+{
+    public static void main(String args[]) {
+            int day=4;
+            switch(day){
+                    case 1:
+                            System.out.println("Saturday");
+                            break;
+                    case 2:
+                            System.out.println("Sunday");
+                            break;
+		    default:
+			    System.out.println("Weekend");
+            }
+
+    }
+}
+
+
+
