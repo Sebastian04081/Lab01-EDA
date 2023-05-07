b40126129c87f00da421cb693359e36fa2f83daa
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-07 13:37:01 -0500

    Agrega un bucle for-each para imprimir los elementos de un arreglo de strings en Exercise05.java

---
+++ Java-Arrays/Exercise05.java
@@ -0,0 +1,8 @@
+public class Exercise05 {
+    public static void main(String args[]) {
+        String[] cars = {"Volvo", "BMW", "Ford"};
+        for(String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
