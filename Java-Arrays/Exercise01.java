23b4f4b10df9e115e4376588df0e9fdc0ffb617c
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-07 13:25:01 -0500

    [200~Agrega un bucle for para imprimir los elementos de un arreglo de strings en Exercise01.java

---
+++ Java-Arrays/Exercise01.java
@@ -0,0 +1,9 @@
+public class Exercise01 {
+    public static void main(String args[]) {
+        String[] cars = {"Volvo", "BMW", "Ford"};
+
+        for(int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
