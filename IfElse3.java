2cd281352b644c5079a1fb82b2fe33323920963d
William <wroquequi@unsa.edu.pe>
2023-05-07 16:03:17 -0500

    Ejercicio 3 de Java If Else

---
+++ IfElse3.java
@@ -0,0 +1,14 @@
+public class IfElse3
+{
+    public static void main(String args[]) {
+            int x=50;
+            int y=50;
+            if(x==y){
+                    System.out.println("Yes");
+            } else {
+		    System.out.println("No");
+	    }
+
+    }
+}
+
