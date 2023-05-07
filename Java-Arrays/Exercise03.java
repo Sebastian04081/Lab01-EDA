70b009223a1072ea585c564b811b59709ad00965
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-07 13:31:34 -0500

    Modifica un elemento específico de un arreglo en Exercise03.java

---
+++ Java-Arrays/Exercise03.java
@@ -0,0 +1,7 @@
+public class Exercise03 {
+    public static void main(String args[]) {
+        String[] cars = {"Volvo", "BMW", "Ford"};
+        cars[0] = "Opel";
+        System.out.println(cars[0]);
+    }
+}
