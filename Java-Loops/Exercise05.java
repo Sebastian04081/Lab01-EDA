589664faf4dcdf15ce720b5bfc6a7afc4fac19cc
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-07 12:39:33 -0500

    Actualiza el código del bucle for en Exercise05.java

---
+++ Java-Loops/Exercise05.java
@@ -0,0 +1,10 @@
+public class Exercise05 {
+    public static void main(String args[]) {
+        for(int i = 0; i < 10; i++) {
+	    if(i == 5) {
+	        break;
+	    }
+	    System.out.println("El valor de i es: " + i);
+	}
+    }
+}
