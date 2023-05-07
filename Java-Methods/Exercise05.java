ce43dc8382cad84e1966c7671cdf5847e5a5f0c6
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-07 14:17:26 -0500

    Agrega un método que utiliza una estructura de control if-else en Exercise05.java

---
+++ Java-Methods/Exercise05.java
@@ -0,0 +1,13 @@
+public class Exercise05 {
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("Access denied");     
+        } else {
+            System.out.println("Access granted"); 
+        }
+    } 
+
+    public static void main(String[] args) { 
+        checkAge( 20);
+    }
+}
