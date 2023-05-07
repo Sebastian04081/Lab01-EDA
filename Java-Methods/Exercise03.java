edcebde6afa397dfab09cb59ead6ea2901dfadbc
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-07 14:00:52 -0500

    Agrega un parámetro al método y lo llama desde el método main en Exercise03.java

---
+++ Java-Methods/Exercise03.java
@@ -0,0 +1,9 @@
+public class Exercise03 {
+    static void myMethod(String fname) {
+        System.out.println(fname + " Doe");
+    }
+
+    public static void main(String[] args) {
+        myMethod("John");
+    }
+}
