710f1192506f93c509cb64d6cf243ae396842d94
William <wroquequi@unsa.edu.pe>
2023-05-07 15:56:34 -0500

    Ejercicio 1 de Java If Else

---
+++ IfElse1.java
@@ -0,0 +1,12 @@
+public class IfElse1
+{
+    public static void main(String args[]) {
+            int x=50;
+            int y=10;
+            if(x>y){
+                    System.out.println("Hello World");
+            }
+
+    }
+}
+
