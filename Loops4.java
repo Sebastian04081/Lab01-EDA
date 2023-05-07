864fcd18538421fa078a5f23f246702b23d65225
William <wroquequi@unsa.edu.pe>
2023-05-07 16:30:42 -0500

    Ejercicio 4 de Java Loops

---
+++ Loops4.java
@@ -0,0 +1,10 @@
+public class Loops1
+{
+    public static void main(String args[]) {
+	    String[] cars={"Volvo","BMW","Ford"};
+	    for (String i: cars){
+		    System.out.println(i);
+	    }
+    }
+}
+
