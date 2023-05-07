4a7c19d4723dad0330f2c0aa37b85f224906e3fb
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-07 13:08:42 -0500

    Refactoriza el bucle for en Exercise06.java para omitir el valor de i=4

--- Java-Loops/Exercise06.java
+++ Java-Loops/Exercise06.java
@@ -1,8 +1,8 @@
-public class Exercise05 {
+public class Exercise06 {
     public static void main(String args[]) {
         for(int i = 0; i < 10; i++) {
-			if(i == 5) {
-				break;
+			if(i == 4) {
+				continue;
 			}
 			System.out.println("El valor de i es: " + i);
 		}
